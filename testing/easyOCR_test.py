cdea348094c9ea52b56c8e0d4d967eb03b07427c
harold <haroldulrich@protonmail.com>
2024-07-08 15:52:23 -0700

    test image enlargement

--- testing/easyOCR_test.py
+++ testing/easyOCR_test.py
@@ -1,11 +1,12 @@
 import os
 import cv2
+import numpy as np
 import easyocr
 
 
 def easyOCR_main():
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    image_file = os.path.join(script_dir, 'test_frames/test_frame_2.jpg')
+    image_file = os.path.join(script_dir, 'test_frames/test_frame_1.jpg')
 
     if os.path.exists(image_file):
         # Load the image with OpenCV
@@ -13,15 +14,23 @@ def easyOCR_main():
         
         # Check if the image was successfully loaded
         if frame is not None:
-            # Initialize the EasyOCR Reader
-            reader = easyocr.Reader(['en'])  # Specify the language, 'en' for English
+            # Specify the language, 'en' for English
+            reader = easyocr.Reader(['en'])
             
             # Convert the image to grayscale as EasyOCR may perform better on such images
-            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             
-            # Apply OCR directly to the grayscale image
-            results = reader.readtext(gray_frame)
+            # Enlarge the image by a factor (e.g., 2 times)
+            scale_factor = 2
+            width = int(frame.shape[1] * scale_factor)
+            height = int(frame.shape[0] * scale_factor)
+            dimensions = (width, height)
+            enlarged_frame = cv2.resize(frame, dimensions, interpolation=cv2.INTER_LINEAR)
             
+            
+            
+            # Apply OCR directly to the frame, greyscale, etc.
+            results = reader.readtext(enlarged_frame)
             # Combine the extracted text into a single string
             text = '\n'.join([result[1] for result in results])
 
